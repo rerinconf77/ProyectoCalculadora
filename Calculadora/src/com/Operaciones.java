84d08175b5493e83d4e5bdc26ff2e31aea7d6ee1
rerinconf77 <rrincon@itstequila.edu.mx>
2018-08-16 09:39:53 -0500

    Se agrego comentarios al archivo Operaciones.java

--- Calculadora/src/com/Operaciones.java
+++ Calculadora/src/com/Operaciones.java
@@ -3,13 +3,15 @@ package com;
 public class Operaciones {
 
     public double Suma(int x, int y) {
-        double resultado;
+        //Metodo para sumar
+	double resultado;
         resultado = x + y;
         return resultado;
     }
     
     public double Resta(int x, int y) {
-        double resultado;
+        //Metodo para restar
+	double resultado;
         resultado = x - y;
         return resultado;
     }
